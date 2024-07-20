40b5f42b8beae690c2d8c2685a4668a13653c58a
GrishaMarkaryan <grisha-markaryan@mail.ru>
2024-07-20 12:50:27 +0300

    next.config.mjs

--- next.config.mjs
+++ next.config.mjs
@@ -1,15 +1,13 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {};
-
-export default nextConfig;
-
-module.exports = {
+const nextConfig = {
     async rewrites() {
       return [
         {
           source: '/yandex_6b19dac3327e96af.html',
-          destination: '/public/yandex_6b19dac3327e96af.html',
+          destination: '/yandex_6b19dac3327e96af.html',
         },
       ];
     },
   };
+  
+  export default nextConfig;
