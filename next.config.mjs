23606416bcab4900fbeee14eac39a2c2f43ee030
GrishaMarkaryan <grisha-markaryan@mail.ru>
2024-07-20 12:13:37 +0300

    added yandex verification file

--- next.config.mjs
+++ next.config.mjs
@@ -2,3 +2,14 @@
 const nextConfig = {};
 
 export default nextConfig;
+
+module.exports = {
+    async rewrites() {
+      return [
+        {
+          source: '/yandex_6b19dac3327e96af.html',
+          destination: '/public/yandex_6b19dac3327e96af.html',
+        },
+      ];
+    },
+  };
--- pages/_document.tsx
+++ pages/_document.tsx
@@ -6,8 +6,6 @@ class MyDocument extends Document {
       <Html>
         <Head>
           <meta name="viewport" content="width=device-width, initial-scale=1" />
-          <meta http-equiv="Content-Type" content="text/html; charset=UTF-8" />
-          <meta name="yandex-verification" content="6b19dac3327e96af" />
         </Head>
         <body>
           <Main />
