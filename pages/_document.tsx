499c7984fa10f645e80809932227c08de27fa39f
GrishaMarkaryan <grisha-markaryan@mail.ru>
2024-07-20 15:40:36 +0300

    installed next-sitemap

--- pages/_document.tsx
+++ pages/_document.tsx
@@ -5,6 +5,9 @@ class MyDocument extends Document {
     return (
       <Html>
         <Head>
+          <title> СТРОЙПЛИТ </title>
+          <meta name="description" content="Купить тротуарную плитку и брусчатку в Сергиевом Посаде. Стройплит предлагает качественные строительные материалы по выгодным ценам." />
+          <meta name="keywords" content="стройплит, стройплит сергиев посад, тротуарная плитка, брусчатка, цемент, бордюр, бордюр дорожный, бордюр тротураный, блоки, блок строительный, блок керамзитный, блок пустотелый, блок полнотелый" />
           <meta name="viewport" content="width=device-width, initial-scale=1" />
         </Head>
         <body>
